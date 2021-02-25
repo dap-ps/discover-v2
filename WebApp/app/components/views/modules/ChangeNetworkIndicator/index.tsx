fde251d909668982761df36636624b216493f351
Richard Ramos <info@richardramos.me>
2021-02-25 14:21:45 -0400

    fix: show a different message when web3 is not available

--- WebApp/app/components/views/modules/ChangeNetworkIndicator/index.tsx
+++ WebApp/app/components/views/modules/ChangeNetworkIndicator/index.tsx
@@ -12,10 +12,10 @@ import {
   WithStyles,
   Typography,
 } from '@material-ui/core';
-import { makeSelectNetworkValid } from 'domain/App/selectors';
+import { makeSelectNetworkValid, makeSelectNetwork } from 'domain/App/selectors';
 import { useSelector } from 'react-redux';
 import classNames from 'classnames';
-import { getNetworkName } from 'domain/App/blockchainUtils';
+import { getNetworkName, NoNetworkIdAvailable } from 'domain/App/blockchainUtils';
 import { uiConstants, appColors } from 'theme';
 import BlockchainConfig from 'embarkArtifacts/config/blockchain.json';
 
@@ -31,8 +31,6 @@ const styles = (theme: Theme) =>
       right: 0,
       transform: 'translate(100%, 0)',
       bottom: '10vh',
-      backgroundColor: appColors.general.red.base,
-      // backgroundColor: brandColors.default.main,
       borderRadius: 15,
       padding: 20,
       color: appColors.general.white.base,
@@ -43,16 +41,34 @@ const styles = (theme: Theme) =>
         transform: 'translate(0, 0)',
       },
     },
+    red: {
+      backgroundColor: appColors.general.red.base,
+    },
+    blue: {
+      backgroundColor: appColors.general.blue.base
+    },
   });
 
 interface OwnProps extends WithStyles<typeof styles> {}
 
 const ChangeNetworkIndicator: React.FC<OwnProps> = ({ classes }: OwnProps) => {
   const correctNetwork = useSelector(makeSelectNetworkValid);
+  const network = useSelector(makeSelectNetwork);
   const correctNetworkName = getNetworkName(BlockchainConfig.networkId);
+
+  if(network === NoNetworkIdAvailable){
+    return (
+      <article
+        className={classNames(classes.root, "show", classes.blue)}
+      >
+        <Typography>You need to connect to a web3 wallet to perform this action. Please consider downloading <a href="https://status.im/get/" target="_blank" style={{color: "#fff"}}>Status.im</a></Typography>
+      </article>
+    );
+  }
+
   return (
     <article
-      className={classNames(classes.root, {
+      className={classNames(classes.root, classes.red, {
         ['show']: !correctNetwork,
       })}
     >
